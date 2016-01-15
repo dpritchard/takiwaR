e1e6d7b73662b844d29870652bb0c0cc1232274e
Daniel Pritchard <daniel@pritchard.co>
2016-01-15 13:15:07 +1300

    New 'sum_on_col' function

--- R/support_funcs.R
+++ R/support_funcs.R
@@ -24,21 +24,6 @@ make_key <- function(string, subs = "takR"){
     return(key)
 }
 
-error_bar <- function(x, y, upper, lower=upper, length=0.05, dir='y', ...){
-    if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
-        stop("Vectors must be same length")
-    if(all(is.factor(x))){
-        x <- as.numeric(x)
-    }
-    if(dir=='y'){
-        suppressWarnings(arrows(x,y+upper,x,y-lower, angle=90, code=3, length=length, ...))
-    } else if(dir=='x') {
-        suppressWarnings(arrows(x-lower,y,x+upper,y, angle=90, code=3, length=length, ...))
-    } else {
-        stop('Impossible')
-    }
-}
-
 se <- function(x, na.rm=FALSE){
     if (!is.vector(x)){
         stop("'x' must be a vector. See ?se for further information.")
@@ -51,7 +36,8 @@ se <- function(x, na.rm=FALSE){
     return(se)
 }
 
-rnt <- function(min = 0, max = 30, by = 1, nrow = 10, ncol = 10, rowpref = "T", colpref = "Q"){
+rnt <- function(min = 0, max = 30, by = 1, nrow = 10, ncol = 10, 
+                rowpref = "T", colpref = "Q"){
     samples <- NULL
     for(a in 1:nrow){
         s1 <- sort(sample(seq(from = min, to = max, by = by), ncol))
@@ -75,53 +61,33 @@ all_is_na <- function(x){
     return(allis)
 }
 
-mtexti <- function(text, side, off = 0.25,
-                   srt = if(side == 2) 90  else
-                       if(side == 4) 270 else 0, ...) {
-    # dimensions of plotting region in user units
-    usr <- par('usr')
-    # dimensions of plotting region in inches
-    pin <- par('pin')
-    # user units per inch
-    upi <- c(usr[2]-usr[1],
-             usr[4]-usr[3]) / pin
-    # default x and y positions
-    xpos <- (usr[1] + usr[2])/2
-    ypos <- (usr[3] + usr[4])/2
-    if(1 == side)
-        ypos <- usr[3] - upi[2] * off
-    if(2 == side)
-        xpos <- usr[1] - upi[1] * off
-    if(3 == side)
-        ypos <- usr[4] + upi[2] * off
-    if(4 == side)
-        xpos <- usr[2] + upi[1] * off
-    text(x=xpos, y=ypos, text, xpd=NA, srt=srt, ...)
+map_attributes <- function(x, out, regex = "^(takRmeta_|takRsec_)") {
+    # Add back attributes beginning with "takRmeta_" or "takRsec_" from `x` to `out`
+    indx <- stringr::str_detect(names(attributes(x)), regex)
+    if(any(indx)){
+        attr_names <- names(attributes(x))[indx]
+        for(a in 1:length(attr_names)){
+            attr(out, attr_names[a]) <- attr(x, attr_names[a])
+        }
+    }
+    return(out)
 }
 
-set_par <- function(brewer.n=8, brewer.name="Dark2", cex.lab=1, cex.main=1.2, cex.axis=1, mar=c(2.5,2.5,1.6,1.1), mgp=c(1.5,.5,0), las=1, ...){
-    par(mar=mar, mgp=mgp, cex.lab=cex.lab, cex.main=cex.main, cex.axis=cex.axis, las=las)
-    par(...)
-    pal <- c("#000000", RColorBrewer::brewer.pal(brewer.n, brewer.name))
-    palette(pal)
-}
-
-set_large_par <- function(brewer.n=8, brewer.name="Dark2", cex.lab=2, cex.main=2, cex.axis=1.5, mar=c(5.1,5.1,3.5,2.1), mgp=c(3,1,0), las=1, ...){
-    par(mar=mar, mgp=mgp, cex.lab=cex.lab, cex.main=cex.main, cex.axis=cex.axis, las=las)
-    par(...)
-    pal <- c("#000000", RColorBrewer::brewer.pal(brewer.n, brewer.name))
-    palette(pal)
-}
-
-setup_plot <- function(x, y, yerr=NA, xlab="", ylab="", ...) {
-    xvals <- c(min(x, na.rm = T), max(x, na.rm = T))
-    yvals <- c(min(y, na.rm = T), max(y, na.rm = T))
-    if(!all(is.na(yerr))){
-        yvals[1] <- yvals[1]-yerr
-        yvals[2] <- yvals[2]+yerr
-    } 
-    plot(xvals, yvals, axes=F, type="n", xlab=xlab, ylab=ylab, ...)
-    box()
-    axis(1, lwd = 0, lwd.ticks = 1)
-    axis(2, lwd = 0, lwd.ticks = 1)
+sum_on_col <- function(x){
+    # Sum columns with matching names
+    # This works equally well if x is a list becuase melt operates by melting the components of a list...
+    x_m <- reshape2::melt(x, as.is=TRUE)
+    x_m[is.na(x_m$value)] <- 0
+    # Cast the data as an array that is row-by-col, summing accross matches....
+    x_out <- reshape2::acast(x_m, Var1~Var2, fun.aggregate=sum)
+    if(inherits(x, "takRwide")){
+        # If it came in as a takRwide, send it back as the same....
+        class(x_out) <- class(x)
+    } else {
+        # Otherwise, send back a takRwide object
+        class(x_out) <- c("takRwide", class(x_out))
+    }
+    # Map any "takR*" attributes
+    x_out <- map_attributes(x, x_out)
+    return(x_out)
 }
